df6c9d40cf208934431ac144a6e88586ed20dbaa
SysAdmin <SysAdmin@DESKTOP-NQU1HM3>
2022-07-09 08:51:26 +0100

    Added function createDriver

--- org.testingbatch.anjali/src/test/java/org/testingbatch/anjali/DependOnTest3.java
+++ org.testingbatch.anjali/src/test/java/org/testingbatch/anjali/DependOnTest3.java
@@ -1,19 +1,21 @@
 package org.testingbatch.anjali;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 
 
 public class DependOnTest3 {
-	
-	WebDriver driver;
-	String baseURl="https://www.toolsqa.com/";
-	System.out.println("Launching Google Chrome browser"); 
-    driver = new ChromeDriver();
-    driver.get(baseUrl);
-    String testTitle = "Free QA Automation Tools For Everyone";
-    String originalTitle = driver.getTitle();
-    Assert.assertEquals(originalTitle, testTitle);
+	public void createDriver() {
+		WebDriver driver = new ChromeDriver();
+		String baseURl = "https://www.toolsqa.com/";
+		System.out.println( "Launching Google Chrome browser" ); 
+    //driver = new ChromeDriver();
+		driver.get(baseURl);
+		String testTitle = "Free QA Automation Tools For Everyone";
+		String originalTitle = driver.getTitle();
+		assert(originalTitle == testTitle);
+	}
 	
 	@BeforeMethod
 	public void beforementhod()
